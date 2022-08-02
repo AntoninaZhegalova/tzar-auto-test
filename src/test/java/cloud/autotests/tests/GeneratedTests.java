e495a99de449c12d3ec8853b76e701bafc230347
Antonina <9616963@gmail.com>
2022-08-02 16:53:05 +0300

    added 5th successful test

--- src/test/java/cloud/autotests/tests/GeneratedTests.java
+++ src/test/java/cloud/autotests/tests/GeneratedTests.java
@@ -1,7 +1,6 @@
 package cloud.autotests.tests;
 
 import cloud.autotests.helpers.DriverUtils;
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,15 +14,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class GeneratedTests extends TestBase {
 
     @Test
-    @DisplayName("Rus-Eng")
+    @DisplayName("Rus-Eng switcher")
     void languageSwitcherTest() {
-        step("Open home page", () -> {
-            open("/");
-        });
+        step("Open home page", () -> open("/"));
 
-        step("Set Eng", () -> {
-            $(".language-switcher-language-url").click();
-        });
+        step("Set Eng", () -> $(".language-switcher-language-url").click());
 
         step("Title should have text: Tsarskoe Selo State Museum and Heritage Site", () -> {
             $("#block-tzar-theme-branding").shouldHave(text("Tsarskoe Selo State Museum and Heritage Site"));
@@ -45,6 +40,38 @@ public class GeneratedTests extends TestBase {
     }
 
     @Test
+    @DisplayName("Open e-ticket page from secondary-menu")
+    void ticketPageTest() {
+        step("Open home page", () -> open("/"));
+
+        step("Set button 'Билеты' from 'block-magazin-menu'", () -> $("#block-magazin").click());
+
+        step("Set button 'Купить билет'", () -> $(byAttribute("href", "/event/597BFBF1F62946E0EC6768D9326B3407BD417633")).click());
+
+        step("Title should have text: Экскурсионное обслуживание предоставляется при формировании группы от 15 до 20 человек.", () -> {
+            $(".row").shouldHave(text("Екатерининский дворец (Маршрут 1) и посещение парка"));
+        });
+    }
+
+    @Test
+    @DisplayName("Search from secondary-menu")
+    void searchSimpleTest() {
+        step("Open home page", () -> open("/"));
+
+        step("Set search icon", () -> $(".search-toggele").click());
+
+        step("", () -> {
+            $("#edit-keys--2").setValue("екатерининский дворец");
+        });
+
+        step("Submit", () -> $("#edit-submit--2").click());
+
+        step("Title should have text: Искать екатерининский дворец.", () -> {
+            $("#block-tzar-theme-page-title").shouldHave(text("Искать екатерининский дворец"));
+        });
+    }
+
+    @Test
     @DisplayName("Page console log should not have errors")
     void consoleShouldNotHaveErrorsTest() {
         step("Open url 'https://tzar.ru/'", () ->
@@ -57,42 +84,4 @@ public class GeneratedTests extends TestBase {
             assertThat(consoleLogs).doesNotContain(errorText);
         });
     }
-
-    @Test
-    @DisplayName("Open e-ticket page from block-magazin-menu")
-    void generatedTest() {
-        step("Open home page", () -> {
-            open("/");
-        });
-
-        step("Set button 'Билеты' from 'block-magazin-menu'", () -> {
-            $("#block-magazin").click();
-        });
-
-        step("Set button 'Купить билет'", () -> {
-            $(byAttribute("href", "/event/597BFBF1F62946E0EC6768D9326B3407BD417633")).click();
-        });
-
-        step("Title should have text: Экскурсионное обслуживание предоставляется при формировании группы от 15 до 20 человек.", () -> {
-            $(".row").shouldHave(text("Екатерининский дворец (Маршрут 1) и посещение парка"));
-        });
-    }
-
-//    @Test
-//    @DisplayName("Eng-Rus")
-//    void generatedTestHh() {
-//        step("Open home page", () -> {
-//            open("/employer");
-//        });
-//
-//        step("Registration page open correct", () -> {
-//            $("#supernova-navi-item supernova-navi-item_lvl-2 " +
-//                    "supernova-navi-item_no-mobile supernova-navi-item_dashboard").click();
-//        });
-//
-//        step("Title should have text: Регистрация работодателя", () -> {
-//            $("#light-page-content__title light-page-content__title_labeled-form").
-//                    shouldHave(text("Регистрация работодателя"));
-//        });
-//    }
 }
